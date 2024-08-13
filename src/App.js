ae5ecdc2bf5d62bb8689e8baf853a7584fd974fe
SaloniSonii <sonisaloni456@gmail.com>
2024-08-14 00:15:26 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import TextForm from './components/TextForm';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 
 function App() {
+  const[mode, setMode] = useState('dark');
+  
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor = '#042743';
+      document.title='Textutils-Dark Mode';
+     
+     // setInterval(()=>{
+       // document.title='Textutils is amazing Mode';
+
+      //},2000);
+      //setInterval(()=>{
+        //document.title='Install Textutils ';
+
+      //},2000);
+      
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor = 'white';
+      document.title='Textutils-Light Mode';
+      
+
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+    <Router>
+    <Navbar title="TextUtils" mode={mode} toggleMode ={toggleMode}/>     
+      <Routes>
+   <Route path='/' element={<div className="container my-8">
+   <TextForm heading="Enter the text to analyze below" mode={mode}/>
+   </div>}/>
+      <Route path='/about' element={<About/>}/>
+      </Routes>
+   </Router> 
+   </div>
+  )
 }
 
 export default App;
